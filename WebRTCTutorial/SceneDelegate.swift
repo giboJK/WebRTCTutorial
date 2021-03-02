ff5df8413977abdc02c5b99072a5e24e643d788c
gibo jk <teoeirm@gmail.com>
2021-03-02 17:01:29 +0900

    Camera session

--- WebRTCTutorial/SceneDelegate.swift
+++ WebRTCTutorial/SceneDelegate.swift
@@ -17,37 +17,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = buildMainViewController()
+        window?.rootViewController = MainViewController()
         window?.makeKeyAndVisible()
     }
     
-    private func buildMainViewController() -> UIViewController {
-        
-        let webRTCClient = WebRTCClient(iceServers: self.config.webRTCIceServers)
-        let signalClient = self.buildSignalingClient()
-        let mainViewController = MainViewController(signalClient: signalClient, webRTCClient: webRTCClient)
-        let navViewController = UINavigationController(rootViewController: mainViewController)
-        if #available(iOS 11.0, *) {
-            navViewController.navigationBar.prefersLargeTitles = true
-        }
-        else {
-            navViewController.navigationBar.isTranslucent = false
-        }
-        return navViewController
-    }
-    
-    private func buildSignalingClient() -> SignalingClient {
-        
-        // iOS 13 has native websocket support. For iOS 12 or lower we will use 3rd party library.
-        let webSocketProvider: WebSocketProvider
-        
-        if #available(iOS 13.0, *) {
-            webSocketProvider = NativeWebSocket(url: self.config.signalingServerUrl)
-        } else {
-            webSocketProvider = StarscreamWebSocket(url: self.config.signalingServerUrl)
-        }
-        
-        return SignalingClient(webSocket: webSocketProvider)
-    }
 }
 
