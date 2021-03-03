736a0b70fbc92bac85f05af4e175e462c826883a
gibo jk <teoeirm@gmail.com>
2021-03-03 16:53:01 +0900

    git rm pods

--- Pods/Target Support Files/Pods-WebRTCTutorial-WebRTCTutorialUITests/Pods-WebRTCTutorial-WebRTCTutorialUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WebRTCTutorial_WebRTCTutorialUITests : NSObject
-@end
-@implementation PodsDummy_Pods_WebRTCTutorial_WebRTCTutorialUITests
-@end
--- Pods/Target Support Files/Pods-WebRTCTutorialTests/Pods-WebRTCTutorialTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WebRTCTutorialTests : NSObject
-@end
-@implementation PodsDummy_Pods_WebRTCTutorialTests
-@end
